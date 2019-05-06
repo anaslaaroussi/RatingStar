386bed6d219338e390510ec678f2991f48860e2e
anaslaaroussialami@gmail.com <anaslaaroussialami@gmail.com>
2019-05-06 15:05:07 +0000

    add rating systel

--- rating.js
+++ rating.js
@@ -1,35 +1,53 @@
-function renderSvg(id, starSize) {
+var fillColor = "orange";
+
+function renderSvg(id, starSize, filled) {
+    var fill = filled ? fillColor : "none";
     return "<?xml version=\"1.0\" encoding= \"utf-8\"?>" +
-        "<svg xmlns=\"http://www.w3.org/2000/svg\"  height=\"" + starSize + "\" viewBox=\"0 0 51 48\" " + "class=\"star\"" + "id=star" + id + ">" +
+        "<svg xmlns=\"http://www.w3.org/2000/svg\"     height=\"" + starSize + "\" viewBox=\"0 0 51 48\" " + "class=\"star\"" + "id=star" + id + ">" +
         "<title>Five Pointed Star</title>" +
-        " <path fill=\"none\" stroke=\"#000\" d=\"m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z\"/>" +
+        " <path fill=" + fill + " stroke=\"#000\" d=\"m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z\"/>" +
         " </svg>"
 }
 
-function initStarFill() {
+function initStarFill(rating) {
+
     var s = document.querySelectorAll(".star");
     s.forEach(function (st) {
-        st.childNodes[2].style.fill = null;
+
+        st.childNodes[2].style.fill = "none";
+
+
     })
 }
 
 
-function returnStar(count, starSize) {
+function returnStar(count, starSize, rating) {
+    var r = count > rating ? rating : null;
     var content = "";
+    var fill = false;
     for (var i = 1; i <= count; i++) {
-        content += renderSvg(i, starSize)
+        if (i <= r) {
+            fill = true
+        } else {
+            fill = false;
+        }
+
+        content += renderSvg(i, starSize, fill);
+
     }
+    initStarFill(r);
+
     var target = document.getElementById("r--s");
     target.innerHTML = content;
     var stars = document.querySelectorAll(".star");
-
     stars.forEach(function (star) {
         star.addEventListener("click", function () {
-            initStarFill();
+
+            initStarFill(null);
             var starId = this.id.split("")[4];
             for (var i = 1; i <= starId; i++) {
                 var t = document.querySelector("#star" + i);
-                t.childNodes[2].style.fill = "orange";
+                t.childNodes[2].style.fill = fillColor;
             }
         });
     })
